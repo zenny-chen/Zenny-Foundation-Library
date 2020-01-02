1e5bbfd06c643537ad21b8824ca9f6230e75a436
Zenny Chen <zenny_chen@sina.com>
2020-01-02 18:22:39 +0800

    Add files via upload

--- zenny_foundation/zf_sys/zf_complex.h
+++ zenny_foundation/zf_sys/zf_complex.h
@@ -59,17 +59,42 @@ static inline zf_ldouble_complex zf_csubl(zf_ldouble_complex a, zf_ldouble_compl
 // complex mul
 static inline zf_float_complex zf_cmulf(zf_float_complex a, zf_float_complex b)
 {
-    return ZF_FLOAT_COMPLEX(a._Val[0] * b._Val[0] - a._Val[1] * b._Val[1], a._Val[0] * b._Val[1] + a._Val[1] * b._Val[0]);
+    return ZF_FLOAT_COMPLEX(a._Val[0] * b._Val[0] - a._Val[1] * b._Val[1], 
+        a._Val[0] * b._Val[1] + a._Val[1] * b._Val[0]);
 }
 
 static inline zf_double_complex zf_cmul(zf_double_complex a, zf_double_complex b)
 {
-    return ZF_DOUBLE_COMPLEX(a._Val[0] * b._Val[0] - a._Val[1] * b._Val[1], a._Val[0] * b._Val[1] + a._Val[1] * b._Val[0]);
+    return ZF_DOUBLE_COMPLEX(a._Val[0] * b._Val[0] - a._Val[1] * b._Val[1], 
+        a._Val[0] * b._Val[1] + a._Val[1] * b._Val[0]);
 }
 
 static inline zf_ldouble_complex zf_cmull(zf_ldouble_complex a, zf_ldouble_complex b)
 {
-    return ZF_LONG_DOUBLE_COMPLEX(a._Val[0] * b._Val[0] - a._Val[1] * b._Val[1], a._Val[0] * b._Val[1] + a._Val[1] * b._Val[0]);
+    return ZF_LONG_DOUBLE_COMPLEX(a._Val[0] * b._Val[0] - a._Val[1] * b._Val[1], 
+        a._Val[0] * b._Val[1] + a._Val[1] * b._Val[0]);
+}
+
+// complex div
+static inline zf_float_complex zf_cdivf(zf_float_complex a, zf_float_complex b)
+{
+    const float coeff = 1.0f / (b._Val[0] * b._Val[0] + b._Val[1] * b._Val[1]);
+    return ZF_FLOAT_COMPLEX((a._Val[0] * b._Val[0] + a._Val[1] * b._Val[1]) * coeff,
+        (a._Val[1] * b._Val[0] - a._Val[0] * b._Val[1]) * coeff);
+}
+
+static inline zf_double_complex zf_cdiv(zf_double_complex a, zf_double_complex b)
+{
+    const double coeff = 1.0 / (b._Val[0] * b._Val[0] + b._Val[1] * b._Val[1]);
+    return ZF_DOUBLE_COMPLEX((a._Val[0] * b._Val[0] + a._Val[1] * b._Val[1]) * coeff,
+        (a._Val[1] * b._Val[0] - a._Val[0] * b._Val[1]) * coeff);
+}
+
+static inline zf_ldouble_complex zf_cdivl(zf_ldouble_complex a, zf_ldouble_complex b)
+{
+    const long double coeff = 1.0L / (b._Val[0] * b._Val[0] + b._Val[1] * b._Val[1]);
+    return ZF_LONG_DOUBLE_COMPLEX((a._Val[0] * b._Val[0] + a._Val[1] * b._Val[1]) * coeff,
+        (a._Val[1] * b._Val[0] - a._Val[0] * b._Val[1]) * coeff);
 }
 
 #else
